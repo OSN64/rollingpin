c85396ae40ffccfe03b3b1f1ef8944f0a6d09c04
ravenstar <brett.penhall@griffithuni.edu.au>
2014-10-10 22:53:25 +1000

    fuck view logic

---
+++ assets/js/newOrder.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+    $('#formordermeth').hide();
+    $('#historybutt').hide();
+
+
+    $('#phoneenter').submit(function(e) {
+        var phone = $('[name="phno"]').val();
+        e.preventDefault();
+        // console.log("name = " + phone)
+        socket.get("/Customer/find?phoneno=" + phone , function (response) { 
+            console.log(response)
+            if (jQuery.isEmptyObject(response)) {
+                console.log("create new user")
+                window.location.href = "/customer/register";
+            }else{
+                $('#phoneenter').hide('slow');
+                $('#formordermeth').show('slow');
+                $('#historybutt').show('slow');
+                console.log("get phone no and set form input to it and show button")
+
+                $('[name="customerid"]').val(phone);
+
+            }
+        });
+    });
+}); //ready()
+
+
+// .submit(function() {
+//   // var message = $('#message').val().trim();
+
+//   // if (message != "") {
+//   //   socket.emit('chat message', message);
+//   //   $('#message').val('');
+//   // }
+//   console.log("form submitted")
+// });
--- foreverSails.sh
+++ foreverSails.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-while [ true ] ; do sails lift ; sleep 3 ; done
+while [ true ] ; do sails lift ; sleep 1 ; done
