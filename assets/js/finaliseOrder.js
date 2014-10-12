17b5ba666964db3e8d5951f1aab43ab781e1c1d4
ravenstar <brett.penhall@griffithuni.edu.au>
2014-10-13 02:47:14 +1000

    Unfin.

---
+++ assets/js/finaliseOrder.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+    
+    /* yolo random color effect cause yolo */
+    function yoloColor() {
+        $('.glyphicon').each(function(i, obj) {
+            
+        });
+    }
+
+    (function yolololol() {
+        var rand = Math.round(Math.random() * (3000 - 500)) + 500;
+        setTimeout(function() {
+            yoloColor();
+            yolololol();  
+        }, rand);
+    }());
+    
+});
